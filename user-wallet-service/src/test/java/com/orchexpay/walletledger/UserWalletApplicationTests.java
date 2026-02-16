b9f2e17b1d233ebba11df233fae3dd8c8d03e696
Gourav Singh <gourav.oct@gmail.com>
2026-02-16 16:47:56 +0530

    Base Setup

---
+++ user-wallet-service/src/test/java/com/orchexpay/walletledger/UserWalletApplicationTests.java
@@ -0,0 +1,10 @@
+package com.orchexpay.walletledger;
+
+import org.junit.jupiter.api.Test;
+
+class UserWalletApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
